4b89dc237d23b959a1b471f4bf5ebdbd6a1345e2
Fred Yagofarov <fred-yagofarov13@psu.ru>
2025-01-15 23:40:01 +0500

    букавку патирял

--- Header.h
+++ Header.h
@@ -4,7 +4,7 @@
 using namespace std;
 
 bool isValidNumber(const string& s);
-long long strToNum(const tring& s, bool& valid);
+long long strToNum(const string& s, bool& valid);
 string numToStr(long long n);
 void splitStrToBlocks(long long* blocks, const string& s, int len, int count);
 void multiplyBlocks(long long* a, long long* b, int count);
