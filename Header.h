7316da7d291c39b5deb1f651246fc64457858aea
Fred Yagofarov <fred-yagofarov13@psu.ru>
2025-01-15 23:14:00 +0500

    а ничо что это уже 3 коммит?

--- Header.h
+++ Header.h
@@ -3,10 +3,10 @@
 
 using namespace std;
 
-bool isValidNumber(const std::string& s);
-long long strToNum(const std::string& s, bool& valid);
+bool isValidNumber(const string& s);
+long long strToNum(const tring& s, bool& valid);
 string numToStr(long long n);
-void splitStrToBlocks(long long* blocks, const std::string& s, int len, int count);
+void splitStrToBlocks(long long* blocks, const string& s, int len, int count);
 void multiplyBlocks(long long* a, long long* b, int count);
 void addBlocks(long long* a, long long* b, int count);
 string buildResult(long long* blocks, int count);
